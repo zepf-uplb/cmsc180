875499a98054b6014d05e9c39c3a4c00a2bbc754
zepf-uplb <zepf.uplb@gmail.com>
2016-10-26 13:36:06 +0800

    fix

--- hello.c
+++ hello.c
@@ -6,13 +6,15 @@
 int **createMatrix(int n);
 int printMatrix(int **M, int n);
 int ***divideMatrix(int **M, int t, int n);
+int printSubmatrix(int ***m, int t, int n);
 int freeMatrix(int **M, int n);
+int freeSubMatrix(int ***m, int t, int n);
 
 int main(int argc, char** argv)
 {
-    int myrank, nprocs, **M, n;
+    int myrank, nprocs, **M, ***m, n, t, i;
 
-    n = 8;
+    n = 8, t = 2;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
@@ -20,17 +22,21 @@ int main(int argc, char** argv)
 
 
     //create matrix
-    M = createMatrix(n);
+    M = createMatrix(n);    
+    printf("Matrix:\n");
     printMatrix(M, n);
-
+    
+    
     //divide to submatrix
-
-
+    m = divideMatrix(M, t, n);
+    printf("Submatrices:\n");
+    printSubmatrix(m, t, n);
     //start time
 
     freeMatrix(M, n);
+    freeSubMatrix(m, t, n);
 
-    printf("Hello from processor %d of %d\n", myrank, nprocs);
+    //printf("Hello from processor %d of %d\n", myrank, nprocs);
 
     MPI_Finalize();
 
@@ -46,7 +52,7 @@ int **createMatrix(int n)
 	for(i = 0; i < n; i++){
 		M[i] = (int*)malloc(sizeof(int)*n);
 		for(j = 0; j < n; j++){			
-			M[i][j] = (rand() % 10)+1;
+			M[i][j] = (rand() % 9)+1;
 		}
 	}	
 
@@ -97,6 +103,29 @@ int ***divideMatrix(int **M, int t, int n)
 	return m;
 }
 
+int printSubmatrix(int ***m, int t, int n)
+{
+	int i, j, k, divs;
+
+	divs = n/t;
+
+	if(n > divs*t){
+		divs++;
+	}
+
+	for(i = 0; i < t; i++){
+		for(j = 0; j < n; j++){
+			for(k = 0; k < divs; k++){
+				printf("%d ", m[i][j][k]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+
+	return 1;
+}
+
 int freeMatrix(int **M, int n)
 {
 	int i;
@@ -106,6 +135,21 @@ int freeMatrix(int **M, int n)
 	}
 
 	free(M);
+
+	return 1;
+}
+
+int freeSubMatrix(int ***m, int t, int n)
+{
+	int i, j;
+
+	for(i = 0; i < t; i++){
+		for(j = 0; j < n; j++){
+			free(m[i][j]);
+		}
+		free(m[i]);
+	}
+	free(m);
 
 	return 1;
 }
